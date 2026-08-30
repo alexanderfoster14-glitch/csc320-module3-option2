e58857e7f15641d882b2ee4814cb8e47ea7fe1bb
Alex Foster <alexander.foster@csuglobal.edu>
2026-08-30 15:54:27 -0600

    Added assignment description

--- csc320_module3_option2/src/module3_option2_grocerybill.java
+++ csc320_module3_option2/src/module3_option2_grocerybill.java
@@ -1,6 +1,29 @@
 /**
- * 
- */
+
+Option #2: Calculate Grocery Bill
+Create a program that can be used to calculate the average weekly and
+	monthly grocery bill for a family of four:
+
+	Prompt the user for the coupon amount as a decimal (example, .10).
+	Ensure the value is set to 10% if the value exceeds 100% or is
+		less than or equal to zero.
+	Prompt the user for weeks 1-4 grocery bills.
+	Calculate the monthly and weekly average for groceries.
+	Display monthly total and weekly average without the coupon.
+	Display monthly total and weekly average with the coupon.
+	Compile and submit your pseudocode, source code, and screenshots
+		of the application executing the application, the results and
+		GIT repository in a single document.
+
+
+**/
+
+/*
+Pseudo Code Outline
+
+
+*/
+
 public class module3_option2_grocerybill {
 	
 	public static void main(String[] args) {
